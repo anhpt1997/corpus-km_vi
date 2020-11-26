873f93bc0a959154594a48f8a3a32edb61eb9447
anhpt1997 <tuananhphan97bn@gmail.com>
2020-11-27 01:50:13 +0700

    fdf

---
+++ read_doc_from_file.py
@@ -0,0 +1,17 @@
+def readDocsFromFile(file):
+
+    with open(file , 'r') as f_r:
+        lines = f_r.readlines()
+        lines =[line.replace("\n","") for line in lines]
+    
+    idDocs, sentences  = [] , []
+    for line in lines:
+        name , sentence = line.split("\t")[0] , line.split("\t")[1]
+        _, id_doc, id_sent = name.split(".")[0] , name.split(".")[1] , name.split(".")[2]
+        idDocs.append([id_doc , id_sent])
+        sentences.append(sentence)
+
+    print(lines[:1])
+
+file ='data_khm.txt'
+readDocsFromFile(file)
